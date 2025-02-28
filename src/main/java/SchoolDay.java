dfe6d4b71936139c247a40ea614d2495c163a624
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-02-28 17:34:36 +0900

    feat : 입력 날짜가 출석 가능한지 여부를 확인할 수 있다

---
+++ src/main/java/SchoolDay.java
@@ -0,0 +1,44 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public enum SchoolDay {
+
+    MONDAY(true, 13, 18),
+    TUESDAY(true, 10, 18),
+    WEDNESDAY(true, 10, 18),
+    THURSDAY(true, 10, 18),
+    FRIDAY(true, 10, 18),
+    SATURDAY(false, 10, 18),
+    SUNDAY(false, 10, 18);
+
+    private static final List<Integer> HOLIDAY = List.of(25);
+
+    private boolean isPossibleAttendance;
+    private int startHour;
+    private int endHour;
+
+    SchoolDay(boolean isPossibleAttendance, int startHour, int endHour) {
+        this.isPossibleAttendance = isPossibleAttendance;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+
+    public boolean isPossibleAttendance() {
+        return isPossibleAttendance;
+    }
+
+    public static boolean isPossibleAttendance(LocalDate date) {
+        return Arrays.stream(SchoolDay.values())
+                .filter(schoolDay -> equalDayOfWeek(schoolDay, date.getDayOfWeek()))
+                .map(SchoolDay::isPossibleAttendance)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당하는 요일을 찾지 못했습니다"));
+    }
+
+    private static boolean equalDayOfWeek(SchoolDay schoolDay, DayOfWeek dayOfWeek) {
+        return Objects.equals(schoolDay.name(), dayOfWeek.name());
+    }
+}
---
+++ src/test/java/SchoolDayTest.java
@@ -0,0 +1,36 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SchoolDayTest {
+
+    @DisplayName("입력 날짜가 출석 가능다면 true를 반환한다")
+    @Test
+    void isPossibleAttendance() {
+        // given
+        LocalDate localDate = LocalDate.of(2024, 12, 13);
+
+        // when
+        boolean isPossibleAttendance = SchoolDay.isPossibleAttendance(localDate);
+
+        // then
+        assertThat(isPossibleAttendance).isTrue();
+    }
+
+    @DisplayName("입력 날짜가 출석 가능하지 않다면 false를 반환한다")
+    @Test
+    void isNotPossibleAttendance() {
+        // given
+        LocalDate localDate = LocalDate.of(2024, 12, 14);
+
+        // when
+        boolean isPossibleAttendance = SchoolDay.isPossibleAttendance(localDate);
+
+        // then
+        assertThat(isPossibleAttendance).isFalse();
+    }
+}
