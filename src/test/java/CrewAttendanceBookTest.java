1778c15118df083debe052a367dba2f223b4cea4
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-02-28 16:27:26 +0900

    feat : 크루 이름을 통해 출석 기록을 조회할 수 있다

--- src/test/java/CrewAttendanceBookTest.java
+++ src/test/java/CrewAttendanceBookTest.java
@@ -22,6 +22,21 @@ public class CrewAttendanceBookTest {
                 () -> assertThat(crewAttendanceBook.retrieveAttendanceRecordsByName(anotherName))
         ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 등록되지 않은 크루입니다.");
+    }
 
+    @DisplayName("크루 이름을 통해 출석 기록을 조회할 수 있다")
+    @Test
+    void retrieveAttendanceRecordsByName() {
+        // given
+        String crewName = "웨이드";
+        AttendanceRecords attendanceRecords = AttendanceRecords.create(List.of());
+        CrewAttendance crewAttendance = CrewAttendance.create(crewName, attendanceRecords);
+        CrewAttendanceBook crewAttendanceBook = CrewAttendanceBook.create(List.of(crewAttendance));
+
+        // when
+        AttendanceRecords foundRecords = crewAttendanceBook.retrieveAttendanceRecordsByName(crewName);
+
+        // then
+        assertThat(foundRecords).isSameAs(attendanceRecords);
     }
 }
