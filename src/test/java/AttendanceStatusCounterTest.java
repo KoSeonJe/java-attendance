a82587796185180f11dcf917ac0fbf9efab5cac2
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-02-28 15:57:13 +0900

    feat : 원하는 출석 상태의 누적 횟수를 조회한다

--- src/test/java/AttendanceStatusCounterTest.java
+++ src/test/java/AttendanceStatusCounterTest.java
@@ -1,25 +1,35 @@
 import java.util.HashMap;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceStatusCounterTest {
 
-
-    @DisplayName("출석들의 누적 출석을 조회한다")
+    @DisplayName("원하는 출석 상태의 누적 횟수를 조회한다")
     @Test
     void retrieveAttendanceCount() {
         // given
         Map<AttendanceStatus, Integer> attendanceStatusCounts = new HashMap<>(
-                Map.of(AttendanceStatus.ATTENDANCE, 1)
+                Map.of(
+                        AttendanceStatus.ATTENDANCE, 1,
+                        AttendanceStatus.LATE, 2,
+                        AttendanceStatus.ABSENCE, 3
+                )
         );
         AttendanceStatusCounter attendanceStatusCounter = AttendanceStatusCounter.create(attendanceStatusCounts);
 
         // when
-        int attendanceStatusCount = attendanceStatusCounter.retrieveAttendanceStatusCount(AttendanceStatus.ATTENDANCE);
+        int attendanceCount = attendanceStatusCounter.retrieveAttendanceStatusCount(AttendanceStatus.ATTENDANCE);
+        int lateCount = attendanceStatusCounter.retrieveAttendanceStatusCount(AttendanceStatus.LATE);
+        int absenceCount = attendanceStatusCounter.retrieveAttendanceStatusCount(AttendanceStatus.ABSENCE);
 
         //then
-        Assertions.assertThat(attendanceStatusCount).isEqualTo(1);
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(attendanceCount).isEqualTo(1);
+            softly.assertThat(lateCount).isEqualTo(2);
+            softly.assertThat(absenceCount).isEqualTo(3);
+        });
     }
 }
